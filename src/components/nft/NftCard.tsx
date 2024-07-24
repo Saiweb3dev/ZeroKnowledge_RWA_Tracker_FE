7017329ddbf681538e8c7207c8fda6f52e6adf38
saik434455@gmail.com <saik434455@gmail.com>
2024-07-24 22:13:11 +0530

    nft card for marketplace

---
+++ src/components/nft/NftCard.tsx
@@ -0,0 +1,25 @@
+// components/NFTCard.tsx
+import { FC } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+interface NFTCardProps {
+  id: string;
+  name: string;
+  image: string;
+  price: string;
+}
+
+const NFTCard: FC<NFTCardProps> = ({ id, name, image, price }) => {
+  return (
+    <Link href={`/nft-marketplace/${id}`}>
+      <div className="border rounded-lg p-4 hover:shadow-lg transition">
+        <Image src={image} alt={name} width={200} height={200} />
+        <h3 className="text-lg font-semibold mt-2">{name}</h3>
+        <p className="text-gray-600">{price} ETH</p>
+      </div>
+    </Link>
+  );
+};
+
+export default NFTCard;
