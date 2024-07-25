57759dc358aa20363499f9287ad79b619584e499
saik434455@gmail.com <saik434455@gmail.com>
2024-07-25 14:13:55 +0530

    refactor: removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ZkVerifier from "@/components/ZkVerifier";
+
 import LandingPage from "../components/LandingPage"
 export default function Home() {
   return (
